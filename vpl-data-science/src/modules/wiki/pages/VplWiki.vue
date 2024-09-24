c43872fbe067256f39f6c76c0f7c067f0b2c8f84
jhoan.dominguez <jhoandominguezu@gmail.com>
2024-09-24 05:23:27 -0500

    add: add component wiki

---
+++ vpl-data-science/src/modules/wiki/pages/VplWiki.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col w-[100%] h-[100%]">
+        <iframe
+            class="flex h-[80vh]"
+            src="https://jhoan-dominguez.github.io/moodle_plugin_vpl_wiki/#/"
+            frameborder="0"
+        ></iframe>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
